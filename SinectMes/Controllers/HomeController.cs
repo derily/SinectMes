8ff61f3e1f409b492d68da20a82b1ca2dd67f94f
derily <click.world@hotmail.com>
2018-05-21 23:37:34 +0800

    添加一些页面，作为后续权限控制的功能项

--- SinectMes/Controllers/HomeController.cs
+++ SinectMes/Controllers/HomeController.cs
@@ -35,5 +35,15 @@ namespace SinectMes.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Sub1()
+        {
+            return View();
+        }
+
+        public IActionResult Sub2()
+        {
+            return View();
+        }
     }
 }
